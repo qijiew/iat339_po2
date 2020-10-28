83a583ade5419c9f5b33358b1d99936b668a6cc3
Qijiew <darwinlurker6@gmail.com>
2020-10-28 04:02:46 -0700

    detail logo interactive part push

---
+++ po1style/service-logo-interactive.js
@@ -0,0 +1,42 @@
+
+
+
+function change2() {
+    
+    var pcontent = document.getElementById("detail-logo-coentent");
+   
+    pcontent.innerHTML = "hi!";
+
+}
+
+function change3() {
+    
+    var pcontent = document.getElementById("detail-logo-coentent");
+   
+    pcontent.innerHTML = "hello!";
+
+}
+function change4() {
+    
+    var pcontent = document.getElementById("detail-logo-coentent");
+   
+    pcontent.innerHTML = "Bonjuor!";
+
+}
+
+function change5() {
+    
+    var pcontent = document.getElementById("detail-logo-coentent");
+   
+    pcontent.innerHTML = "안녕!";
+
+}
+
+
+
+function changeback() {
+    var h2content = document.getElementById("logo1");
+    var pcontent = document.getElementById("detail-logo-coentent");
+    
+    pcontent.innerHTML = "Interior design";
+}
