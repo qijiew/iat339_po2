826c72cc9e33431193c13781857e9216429c88ab
Qijiew <darwinlurker6@gmail.com>
2020-10-28 05:50:03 -0700

    unify all the navigation bar with overlay effect

--- po1style/nav.js
+++ po1style/nav.js
@@ -1,18 +1,12 @@
-/*let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
-}); */
 
 
-            function openNav() {
-                document.getElementById(
-                    "myNav").style.width = "100%";
-            }
 
-            function closeNav() {
-                document.getElementById(
-                    "myNav").style.width = "0%";
-            } 
-     
+function openNav() {
+  document.getElementById(
+    "myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById(
+    "myNav").style.width = "0%";
+}
